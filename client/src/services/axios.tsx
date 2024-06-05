283fd5e5d12d28f680cddd3ec5bdaca1ae7a24f4
zulhakim88 <zulfadhlil.hakim@gmail.com>
2024-06-05 22:15:05 +0700

    remove unnecessary axios response interceptor

--- client/src/services/axios.tsx
+++ client/src/services/axios.tsx
@@ -1,11 +1,7 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 
 const service = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
-})
-
-service.interceptors.response.use((response: AxiosResponse): AxiosResponse => {
-    return response
 })
 
 service.interceptors.request.use((request) => {
