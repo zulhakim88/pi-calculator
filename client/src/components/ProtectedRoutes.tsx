0b5b6282d4dd2c844202e61bcdc95444221bcd1f
zulhakim88 <zulfadhlil.hakim@gmail.com>
2024-06-05 16:52:51 +0700

    fix typing in ProtectedRoutes component

--- client/src/components/ProtectedRoutes.tsx
+++ client/src/components/ProtectedRoutes.tsx
@@ -2,13 +2,13 @@ import { UserAuth } from '../context/AuthContext'
 import { Navigate } from 'react-router-dom'
 import { ChildrenElement } from '../lib/types'
 
-const ProtectedRoutes = ({ children }: ChildrenElement) => {
+const ProtectedRoutes = ({ children }: ChildrenElement): JSX.Element | null => {
     const { user } = UserAuth()
 
     if (!user) {
         return <Navigate to="/signin" />
     }
-    return children
+    return children as JSX.Element
 }
 
 export default ProtectedRoutes
