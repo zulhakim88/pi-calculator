35a536a871f2d4981de098f2b4237c69ed56c50c
zulhakim88 <zulfadhlil.hakim@gmail.com>
2024-06-07 13:16:12 +0700

    fix styling in Home component

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -17,10 +17,10 @@ const Home = (): JSX.Element => {
       <div className="grid grid-cols-4 p-3">
         <div className="sm:col-span-4 col-span-full bg-white mb-3 p-4 rounded-md">
           <h1 className="text-2xl font-bold pb-2">Welcome {user.displayName}!</h1>
-          <p className="flex flex-col justify-between overflow-scroll p-4 bg-gray-200 rounded-md max-h-[200px]">
+          <p className="flex flex-col justify-between overflow-scroll p-3 bg-gray-200 rounded-md max-h-[200px]">
             <div>
               <h2 className="font-bold mb-2">Objective</h2>
-              <ol className="list-disc pl-5">
+              <ol className="list-disc pl-5 text-sm">
                 <li>
                   Calculate PI to the Nth decimal point precision.
                 </li>
@@ -31,18 +31,21 @@ const Home = (): JSX.Element => {
             </div>
             <div>
               <h2 className="font-bold mb-2 my-2">Why do we need this?</h2>
-              <ol className="list-disc pl-5">
+              <ol className="list-disc pl-5 text-sm">
                 <li>
                   We don't need this! Refer here: <Link className="underline text-blue-500" to="" target="_blank">How Many Decimals of Pi Do We Really Need?</Link> (We don't actually need more than 15 decimals)
                 </li>
                 <li>
                   Why did I build this then? I was wondering until I can't sleep! So I started building the Calculator 🤣.
                 </li>
+                <li>
+                  Longest PI memorized is <Link className="underline text-blue-500" to="https://www.timesnownews.com/viral/pi-day-2023-meet-rajveer-meena-who-recited-70000-pi-digits-blindfolded-article-98605930#:~:text=In%20the%20endless%20pursuit%20of,Read%20More&text=Rajveer%20Meena%20memorised%20and%20recited,70%2C000%20decimal%20places%20while%20blindfolded." target="_blank">here</Link> (So I'm not the only one wondering 🤪).
+                </li>
               </ol>
             </div>
             <div>
               <h2 className="font-bold my-2">Referance to the implementation</h2>
-              <ol className="list-disc pl-5">
+              <ol className="list-disc pl-5 text-sm">
                 <li>
                   Taken from <Link className="underline text-blue-500" to="https://bellard.org/quickjs/pi.html" target='_blank'>Fabrice Bellard's JS take</Link> on <Link className="underline text-blue-500" to="https://en.wikipedia.org/wiki/Chudnovsky_algorithm" target='_blank'>Chudnovsky's algorithm</Link>.
                 </li>
