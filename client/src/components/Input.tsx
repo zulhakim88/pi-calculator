f28793d212ec415229d110975a9c7c94b6f800df
zulhakim88 <zulfadhlil.hakim@gmail.com>
2024-06-10 19:36:27 +0700

    change transition duration from 150ms to 200ms in Input component

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -27,7 +27,7 @@ const Input = ({ className, label, name, value, onChange, type, required, min }:
 			/>
 			<label
 				htmlFor={label}
-				className={`absolute start-3 top-0 z-10 origin-[0] bg-white px-2 duration-150 hover:cursor-text ${
+				className={`absolute start-3 top-0 z-10 origin-[0] bg-white px-2 duration-200 hover:cursor-text ${
 					value
 						? "top-0 -translate-y-1 scale-75 text-gray-400 peer-focus:text-blue-600"
 						: "peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-placeholder-shown:text-gray-400 peer-focus:top-0 peer-focus:-translate-y-1 peer-focus:scale-75 peer-focus:text-blue-600"
