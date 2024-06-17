e2e4240963fdb61353d62a0f95e337dee1bcea09
zulhakim88 <zulfadhlil.hakim@gmail.com>
2024-06-17 17:48:35 +0700

    add z-index to the Header component so it will be the most top component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,7 +5,7 @@ import { useUserAuth } from "../context/AuthContext"
 const Header = (): JSX.Element => {
 	const { displayName } = useUserAuth()
 	return (
-		<div className="sticky top-0 flex w-screen items-center justify-between bg-white p-3 shadow-md">
+		<div className="sticky top-0 z-50 flex w-screen items-center justify-between bg-white p-3 shadow-md">
 			<div className="flex flex-row items-center">
 				<div className="rounded-md bg-purple-700 p-3 text-white">
 					<FaRocket className="h-7 w-7" />
